31b731b9d586f034430ead39515b107c679cdec7
Corentin Dupont <corentin.dupont@gmail.com>
2018-12-11 22:44:13 +0100

    style

--- src/Waziup/API.hs
+++ src/Waziup/API.hs
@@ -39,8 +39,12 @@ type WaziupDocs = SwaggerSchemaUI "swagger-ui" "swagger.json"
 
 type AuthAPI = 
   "auth" :>  (
-         "permissions" :> Header "Authorization" Token :> Get  '[JSON]      [Perm]
-    :<|> "token"       :> ReqBody '[JSON] AuthBody     :> Post '[PlainText] Token
+        "permissions"
+          :> Header "Authorization" Token
+          :> Get  '[JSON]      [Perm]
+    :<|> "token"
+          :> ReqBody '[JSON] AuthBody
+          :> Post '[PlainText] Token
     )
 
 
@@ -48,48 +52,64 @@ type AuthAPI =
 -- * Devices --
 ---------------
 
-type DevicesAPI = Flat ( 
-  "devices" :> Header "Authorization" Token :> ( 
-          QueryParam "q"      DevicesQuery
-       :> QueryParam "limit"  DevicesLimit
-       :> QueryParam "offset" DevicesOffset
-       :> Get '[JSON] [Device] 
-  :<|> ReqBody '[JSON] Device
-       :> PostNoContent '[JSON] NoContent
-  :<|> DeviceAPI
-    ))
-
-type DeviceAPI = Flat (
-  Capture "device_id" DeviceId :> (                         Get             '[JSON] Device
-    :<|>                                                    DeleteNoContent '[JSON] NoContent
-    :<|> "name"       :> ReqBody '[PlainText] DeviceName :> PutNoContent    '[JSON] NoContent
-    :<|> "location"   :> ReqBody '[JSON]      Location   :> PutNoContent    '[JSON] NoContent
-    :<|> "gateway_id" :> ReqBody '[PlainText] GatewayId  :> PutNoContent    '[JSON] NoContent
-    :<|> "visibility" :> ReqBody '[PlainText] Visibility :> PutNoContent    '[JSON] NoContent
-    ))
+type DevicesAPI = Flat (Header "Authorization" Token :> 
+  "devices" :> ( 
+         QueryParam "q"      DevicesQuery
+      :> QueryParam "limit"  DevicesLimit
+      :> QueryParam "offset" DevicesOffset
+      :> Get '[JSON] [Device] 
+    :<|> ReqBody '[JSON] Device
+      :> PostNoContent '[JSON] NoContent
+    :<|> Capture "device_id" DeviceId :> (
+            Get             '[JSON] Device
+      :<|>  DeleteNoContent '[JSON] NoContent
+      :<|> "name"       
+           :> ReqBody '[PlainText] DeviceName
+           :> PutNoContent    '[JSON] NoContent
+      :<|> "location"
+           :> ReqBody '[JSON] Location
+           :> PutNoContent    '[JSON] NoContent
+      :<|> "gateway_id"
+           :> ReqBody '[PlainText] GatewayId
+           :> PutNoContent    '[JSON] NoContent
+      :<|> "visibility"
+           :> ReqBody '[PlainText] Visibility
+           :> PutNoContent    '[JSON] NoContent
+      )))
 
 
 ---------------
 -- * Sensors --
 ---------------
 
-type SensorsAPI = Flat (
-  "devices" :> Header "Authorization" Token :> Capture "device_id" DeviceId :> 
-  "sensors" :> (                        Get           '[JSON] [Sensor]
-    :<|> ReqBody '[JSON] Sensor      :> PostNoContent '[JSON] NoContent
-    :<|> SensorAPI
-    ))
-
-type SensorAPI = Flat (
-  Capture "sensor_id" SensorId :> (                                Get             '[JSON] Sensor
-    :<|>                                                           DeleteNoContent '[JSON] NoContent
-    :<|> "name"          :> ReqBody '[PlainText] SensorName     :> PutNoContent    '[JSON] NoContent
-    :<|> "sensor_kind"   :> ReqBody '[PlainText] SensorKindId   :> PutNoContent    '[JSON] NoContent
-    :<|> "quantity_kind" :> ReqBody '[PlainText] QuantityKindId :> PutNoContent    '[JSON] NoContent
-    :<|> "unit"          :> ReqBody '[PlainText] UnitId         :> PutNoContent    '[JSON] NoContent
-    :<|> "calib"         :> ReqBody '[JSON] LinearCalib         :> PutNoContent    '[JSON] NoContent
-    :<|> "value"         :> ReqBody '[JSON] SensorValue         :> PostNoContent   '[JSON] NoContent
-    ))
+type SensorsAPI = Flat ( Header "Authorization" Token :> 
+  "devices" :> Capture "device_id" DeviceId :> 
+    "sensors" :> (
+           Get '[JSON] [Sensor]
+      :<|> ReqBody '[JSON] Sensor 
+        :> PostNoContent '[JSON] NoContent
+      :<|> Capture "sensor_id" SensorId :> (
+             Get '[JSON] Sensor
+        :<|> DeleteNoContent '[JSON] NoContent
+        :<|> "name"          
+           :> ReqBody '[PlainText] SensorName
+           :> PutNoContent '[JSON] NoContent
+        :<|> "sensor_kind"  
+           :> ReqBody '[PlainText] SensorKindId
+           :> PutNoContent '[JSON] NoContent
+        :<|> "quantity_kind"
+           :> ReqBody '[PlainText] QuantityKindId
+           :> PutNoContent '[JSON] NoContent
+        :<|> "unit"
+           :> ReqBody '[PlainText] UnitId
+           :> PutNoContent '[JSON] NoContent
+        :<|> "calib"
+           :> ReqBody '[JSON] LinearCalib
+           :> PutNoContent    '[JSON] NoContent
+        :<|> "value"
+           :> ReqBody '[JSON] SensorValue
+           :> PostNoContent   '[JSON] NoContent
+      )))
 
 
 --------------------
@@ -111,70 +131,80 @@ type SensorDataAPI = Flat (
 -- * Actuators --
 -----------------
 
-type ActuatorsAPI = Flat (
-  "devices" :> Header "Authorization" Token :> Capture "device_id" DeviceId :> 
-  "actuators" :> (                    Get           '[JSON] [Actuator]
-    :<|> ReqBody '[JSON] Actuator  :> PostNoContent '[JSON] NoContent
-    :<|> ActuatorAPI
-    ))
-
-type ActuatorAPI = Flat (
-  Capture "actuator_id" ActuatorId :> (                                     Get             '[JSON] Actuator 
-    :<|>                                                                    DeleteNoContent '[JSON] NoContent
-    :<|> "name"                :> ReqBody '[PlainText] ActuatorName      :> PutNoContent    '[JSON] NoContent
-    :<|> "actuator_kind"       :> ReqBody '[PlainText] ActuatorKindId    :> PutNoContent    '[JSON] NoContent
-    :<|> "value_type"          :> ReqBody '[PlainText] ActuatorValueType :> PutNoContent    '[JSON] NoContent
-    :<|> "value"               :> ReqBody '[JSON] Value                  :> PutNoContent    '[JSON] NoContent
-    ))
+type ActuatorsAPI = Flat ( Header "Authorization" Token :> 
+  "devices" :> Capture "device_id" DeviceId :> 
+    "actuators" :> (
+           Get '[JSON] [Actuator]
+      :<|> ReqBody '[JSON] Actuator
+        :> PostNoContent '[JSON] NoContent
+      :<|> Capture "actuator_id" ActuatorId :> (
+             Get '[JSON] Actuator 
+        :<|> DeleteNoContent '[JSON] NoContent
+        :<|> "name"
+          :> ReqBody '[PlainText] ActuatorName
+          :> PutNoContent '[JSON] NoContent
+        :<|> "actuator_kind"
+          :> ReqBody '[PlainText] ActuatorKindId
+          :> PutNoContent '[JSON] NoContent
+        :<|> "value_type"
+          :> ReqBody '[PlainText] ActuatorValueType
+          :> PutNoContent '[JSON] NoContent
+        :<|> "value"
+          :> ReqBody '[JSON] Value
+          :> PutNoContent '[JSON] NoContent
+      )))
 
 
 ----------------
 -- * Gateways --
 ----------------
 
-type GatewaysAPI = Flat ( 
-  "gateways" :> 
-    Header "Authorization" Token :> (
-                                     Get  '[JSON]      [Gateway]
-    :<|>  ReqBody '[JSON] Gateway :> Post '[PlainText] GatewayId
-    :<|>  GatewayAPI
-    ))
-
-type GatewayAPI = (
-  Capture "gw_id" GatewayId :> (                 Get             '[JSON] Gateway
-    :<|>                                         DeleteNoContent '[JSON] NoContent
-    :<|> "tunnel" :> ReqBody '[PlainText] Int :> PutNoContent    '[JSON] NoContent
-    :<|> "tunnel"                             :> DeleteNoContent '[JSON] NoContent
-    ))
+type GatewaysAPI = Flat ( Header "Authorization" Token :> 
+  "gateways" :> (
+          Get  '[JSON] [Gateway]
+    :<|>  ReqBody '[JSON] Gateway
+       :> Post '[PlainText] GatewayId
+    :<|> Capture "gw_id" GatewayId :> (
+           Get '[JSON] Gateway
+      :<|> DeleteNoContent '[JSON] NoContent
+      :<|> "tunnel"
+        :> ReqBody '[PlainText] Int
+        :> PutNoContent '[JSON] NoContent
+      :<|> "tunnel"
+        :> DeleteNoContent '[JSON] NoContent
+    )))
 
 
 ----------------
 -- * Projects --
 ----------------
 
-type ProjectsAPI = Flat ( 
-  "projects" :> 
-    Header "Authorization" Token :> (
-                                     Get  '[JSON]      [Project]
-    :<|>  ReqBody '[JSON] Project :> Post '[PlainText] ProjectId
-    :<|>  ProjectAPI
-    ))
-
-type ProjectAPI = (
-  Capture "id" ProjectId :> (                         Get             '[JSON] Project
-    :<|>                                              DeleteNoContent '[JSON] NoContent
-    :<|> "devices"  :> ReqBody '[JSON] [DeviceId]  :> PutNoContent    '[JSON] NoContent
-    :<|> "gateways" :> ReqBody '[JSON] [GatewayId] :> PutNoContent    '[JSON] NoContent))
+type ProjectsAPI = Flat ( Header "Authorization" Token :> 
+  "projects" :> (
+          Get  '[JSON]      [Project]
+    :<|>  ReqBody '[JSON] Project
+       :> Post '[PlainText] ProjectId
+    :<|> Capture "id" ProjectId :> (
+           Get '[JSON] Project
+      :<|> DeleteNoContent '[JSON] NoContent
+      :<|> "devices"
+        :> ReqBody '[JSON] [DeviceId]
+        :> PutNoContent '[JSON] NoContent
+      :<|> "gateways"
+        :> ReqBody '[JSON] [GatewayId]
+        :> PutNoContent '[JSON] NoContent
+    )))
 
 
 -------------
 -- * Users --
 -------------
 
-type UsersAPI = Flat ( 
-  "users" :> Header "Authorization" Token :> (
-         Get  '[JSON]      [User]
-    :<|> Capture "user_id" ProjectId :> Get '[JSON] User))
+type UsersAPI = Flat ( Header "Authorization" Token :> 
+  "users" :> (
+         Get  '[JSON] [User]
+    :<|> Capture "user_id" ProjectId
+      :> Get '[JSON] User))
 
 
 ------------------
@@ -191,5 +221,6 @@ type OntologiesAPI =
 
 instance ToParamSchema Token where
   toParamSchema _ = binaryParamSchema
+
 instance ToSchema Token where
   declareNamedSchema _ = pure (NamedSchema (Just "Token") binarySchema)
